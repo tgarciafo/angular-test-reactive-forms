f8627a0760ee9c21eb7a2a08d0f94d1b2a0968f9
taniagarcia89 <taniag89@gmail.com>
2020-09-23 12:35:08 +0200

    correction

--- src/app/Directives/check-word.validator.ts
+++ src/app/Directives/check-word.validator.ts
@@ -2,7 +2,7 @@ import { FormControl, ValidatorFn} from '@angular/forms';
 
 export class CheckWord{
     public static checkInvalidWord(mask: RegExp): ValidatorFn{
-        return (control: FormControl): { [key: string]: any | null } => {
+        return (control: FormControl): { [key: string]: any } | null  => {
             const invalid = mask.test(control.value);
             return invalid ? { 'invalidWord': { value: control.value } } : null;
         }
